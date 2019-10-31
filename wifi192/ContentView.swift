6662e0ddc67bf102271853ea1426b79dbf88be7e
Marat Saytakov <remarr@gmail.com>
2019-10-31 21:12:05 +0700

    Initial Commit

---
+++ wifi192/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  wifi192
+//
+//  Created by Marat Saytakov on 31.10.2019.
+//  Copyright © 2019 saytakov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
